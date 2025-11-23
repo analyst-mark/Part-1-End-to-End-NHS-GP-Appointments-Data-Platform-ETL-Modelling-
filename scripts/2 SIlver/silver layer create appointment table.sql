f3158169bb2194e1b5d901c276fa85fddaf252e9
analyst-mark <127795776+analyst-mark@users.noreply.github.com>
2025-11-23 21:09:21 +0000

    Create silver layer create appointment table.sql

---
+++ scripts/2 SIlver/silver layer create appointment table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE silver.appointments (
+   SUB_ICB_LOCATION_CODE        varchar(255) NULL,
+    SUB_ICB_LOCATION_ONS_CODE    varchar(255) NULL,
+    SUB_ICB_LOCATION_NAME        varchar(255) NULL,
+    ICB_ONS_CODE                 varchar(255) NULL,
+    REGION_ONS_CODE              varchar(255) NULL,
+    Appointment_Date             varchar(255) NULL,
+    APPT_STATUS                  varchar(255) NULL,
+    HCP_TYPE                     varchar(255) NULL,
+    APPT_MODE                    varchar(255) NULL,
+    TIME_BETWEEN_BOOK_AND_APPT   varchar(255) NULL,
+    COUNT_OF_APPOINTMENTS        int          NULL
+);
+
+
+
+select * from silver.appointments
